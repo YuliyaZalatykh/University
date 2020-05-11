7273f0aa2522571c4398223d47b32fedaeb324c2
Yuliya Zalatykh <yuliya_zalatykh@outlook.com>
2020-05-11 14:39:01 +0300

    feat: complete task 4

--- Department.Designer.cs
+++ Department.Designer.cs
@@ -30,23 +30,25 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(710, 682);
+            this.dataGridView1.Size = new System.Drawing.Size(877, 360);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(745, 202);
+            this.button1.Location = new System.Drawing.Point(883, 192);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(134, 58);
             this.button1.TabIndex = 1;
@@ -54,16 +56,39 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 409);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(877, 218);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Department
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gold;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Department";
             this.Size = new System.Drawing.Size(1068, 682);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -72,5 +97,7 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Department.cs
+++ Department.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using University.Controller;
+using University.BO;
+using University.Utils;
 
 namespace University
 {
@@ -15,11 +17,15 @@ namespace University
     {
 
         Query controller;
+        Query controller2;
+        Query controller3;
 
         public Department()
         {
             InitializeComponent();
             controller = new Query(ConnectionString.ConnStr);
+            controller2 = new Query(ConnectionString.ConnStr);
+            controller3 = new Query(ConnectionString.ConnStr);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -29,7 +35,61 @@ namespace University
 
         private void button1_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = controller.UpdateTable("Кафедра");
+            DataTable dataTable = controller.UpdateTable("Кафедра");
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int DepartmentCode = int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells["Код_каф"].Value.ToString());
+
+            DataTable specialityTable = controller2.UpdateTable("Специальности");
+            List<BO.Speciality> specialities = new List<BO.Speciality>();
+            
+            foreach(DataRow SpecialityRow in specialityTable.Rows) {
+                int DeptCode = int.Parse(SpecialityRow["Код_каф"].ToString());
+                if(DepartmentCode == DeptCode)
+                {
+                    int SpecCode = int.Parse(SpecialityRow["Код_спец"].ToString());
+                    string Name = SpecialityRow["Назв_спец"].ToString();
+                    string Qualification = SpecialityRow["Квалифик"].ToString();
+                    string StudyForm = SpecialityRow["Форма_обуч"].ToString();
+                    int Duration = int.Parse(SpecialityRow["Продолжительность"].ToString());
+
+                    BO.Speciality speciality = new BO.Speciality(SpecCode, Name, Qualification, StudyForm, DeptCode, Duration);
+                    specialities.Add(speciality);
+                }
+            }
+            
+            
+            DataTable dataTable = controller3.UpdateTable("Дисциплина");
+
+            List<Discipline> disciplines = new List<Discipline>();
+
+            foreach (BO.Speciality speciality in specialities)
+            {
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int SpecCode = int.Parse(row["Код_спец"].ToString());
+                    if (speciality.Code == SpecCode)
+                    {
+                        int Code = int.Parse(row["Код_дисц"].ToString());
+                        string Name = row["Назв_дисц"].ToString();
+                        int Semest = int.Parse(row["Семестры"].ToString());
+                        int Hours = int.Parse(row["Часы"].ToString());
+                        int LabH = int.Parse(row["Лаб_зан"].ToString());
+                        int PractiseH = int.Parse(row["Практ_зан"].ToString());
+                        int CourseH = int.Parse(row["Курсовые"].ToString());
+                        string ReportType = row["Вид_отчет"].ToString();
+
+                        Discipline discipline = new Discipline(Code, Name, Semest, Hours, LabH, PractiseH, CourseH, ReportType, SpecCode);
+                        disciplines.Add(discipline);
+                    }
+                }
+            }
+
+            DataTable dataTable2 = Utils.Utils.ToDataTable(disciplines);
+            dataGridView2.DataSource = dataTable2;
         }
     }
 }
